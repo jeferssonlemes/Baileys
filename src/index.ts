4b0f5844916702a8632c2cc73d0233965c972823
jefersson <jeferssonpt@gmail.com>
2022-03-23 17:01:01 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export { makeWALegacySocket }
 
 export type WASocket = ReturnType<typeof makeWASocket>
 
+// teste
 export type AnyWASocket = WASocket | WALegacySocket
 
 export default makeWASocket
